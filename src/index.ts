6edf88d872838d580a325a99cf51c213953a56bd
xiaoYown <qzy09101018@sina.com>
2021-10-22 00:39:46 +0800

    fix: fix template

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { default as eg0 } from './modules/eg-0';
-export { default as eg1 } from './modules/eg-1';
+export { default as eg0 } from './modules/eg-0/eg-0';
+export { default as eg1 } from './modules/eg-1/eg-1';
